d00fc5fc73ee8666f244e634d9fb21079d13d62b
Ryan Bigg <radarlistener@gmail.com>
2013-07-22 13:07:39 +1000

    Never moderate first post

--- config/initializers/forem.rb
+++ config/initializers/forem.rb
@@ -3,6 +3,7 @@ Forem.email_from_address = "please-change-me@example.com"
 # If you do not want to use gravatar for avatars then specify the method to use here:
 # Forem.avatar_user_method = :custom_avatar_url
 Forem.per_page = 20
+Forem.moderate_first_post = false
 
 
 # Rails.application.config.to_prepare do
